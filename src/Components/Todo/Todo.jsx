2df48fe30bea87a0b7f1b22837ff8d8fe8c131bf
Sveta Mituykhina <s.tu.tu.908@gmail.com>
2020-05-30 18:09:11 +0300

    hooks

--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Card from '@material-ui/core/Card';
 import ItemList from '../ItemList/ItemList';
 import Input from "../Input/Input";
@@ -13,132 +13,137 @@ const buttonTasksName = {
     all: 'Все'
 };
 
-class Todo extends React.Component {
-    maxId = 100;
-    state = {
-        tasks:  JSON.parse(localStorage.getItem('editedList') ||
-            '[{"value":"Привет! Я - твой todo-list","isDone":false, "isImportant": false,"id":1}]'),
-        count: 1,
-        filtered: buttonTasksName.all,
-        classNameForInputRepeat: false,
-    };
+const Todo = () => {
 
-    onDragEnd = result => {
+    const [tasks, setTasks] = useState(
+        [{  value: "Привет! Я - твой todo-list",
+            isDone: false,
+            isImportant: false,
+            id: 1 }]
+    );
+    const [filtered, setFiltered] = useState(  {...buttonTasksName,  all: 'Все'});
+    const [count, setCount] = useState(1);
+    const [classNameForInputRepeat, setClassNameForInputRepeat] = useState(false);
+
+    useEffect(() => {
+        const tasks = localStorage.getItem('tasks');
+        setTasks(JSON.parse(tasks))
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('todoItems', JSON.stringify(tasks))
+    }, [tasks]);
+
+    const onDragEnd = result => {
         const { source, destination } = result;
         if (!destination) return;
 
-        const newTodoItems = this.state.tasks;
+        const newTodoItems = [...tasks];
 
         const [removed] = newTodoItems.splice(source.index, 1);
         newTodoItems.splice(destination.index, 0, removed)
-        this.setState([
+        setTasks([
             ...newTodoItems
         ])
     };
 
-    onClickDone = id => {
-        const newItemList = this.state.tasks.map(item => {
+    const onClickDone = id => {
+        const newItemList = tasks.map(item => {
             const newItem = {...item};
             if (item.id === id) {
                 newItem.isDone = !item.isDone;
             }
             return newItem;
         });
-        this.setState({ tasks: newItemList });
+        setTasks({ tasks: newItemList });
     };
 
-    onClickImportant = id => {
-        const newItemList = this.state.tasks.map(item => {
+    const onClickImportant = id => {
+        const newItemList = tasks.map(item => {
             const newItem = {...item};
             if (item.id === id) {
                 newItem.isImportant = !item.isImportant;
             }
             return newItem;
         });
-        this.setState({ tasks: newItemList });
+        setTasks({ tasks: newItemList });
     };
 
-    onClickDelete = id => {
-        const newItemList = this.state.tasks.filter(item => {
+    const onClickDelete = id => {
+        const newItemList = setTasks(tasks.filter(item => {
             return item.id !== id
-        });
+        }));
 
-        this.setState({ tasks: newItemList });
-        this.setState((count) => count - 1);
+        setTasks({ tasks: newItemList });
+        setCount((count) => count - 1);
     };
 
-    onClickAdd = value => {
-        const item = this.state.tasks.filter(item => item.value === value);
+    const onClickAdd = value => {
+        const item = tasks.filter(item => item.value === value);
         if (item.length === 0) {
-            this.setState(state => ({
+            setTasks(tasks => ({
                 tasks: [
-                    ...state.tasks,
+                    ...tasks,
                     {
                         value,
                         isDone: false,
                         isImportant: false,
-                        id: state.count + 1
+                        id: setCount + 1
                     }
                 ],
-                count: state.count + 1,
-                classNameForInputRepeat: false
+                setCount: count + 1,
+                setClassNameForInputRepeat: false
             }));
         } else {
-            this.setState({ classNameForInputRepeat: true });
+            setClassNameForInputRepeat(true);
             setTimeout(() => {
-                this.setState({
-                    classNameForInputRepeat: false
-                });
+                setClassNameForInputRepeat(false);
             }, 1500);
         }
     };
 
-    onClickFilteredTasks = filtered => this.setState({ filtered: filtered });
-
-    render() {
-        let dealListString = JSON.stringify(this.state.tasks);
-        localStorage.setItem('editedList', dealListString);
+    const onClickFilteredTasks = filtered => setFiltered({ filtered: filtered });
 
         let filteredTasks;
-        switch (this.state.filtered) {
+        switch (filtered) {
             case buttonTasksName.completed:
-                filteredTasks = this.state.tasks.filter(item => item.isDone);
+                filteredTasks = tasks.filter(item => item.isDone);
                 break;
             case buttonTasksName.uncomplited:
-                filteredTasks = this.state.tasks.filter(item => !item.isDone);
+                filteredTasks = tasks.filter(item => !item.isDone);
                 break;
             case buttonTasksName.all:
-                filteredTasks = this.state.tasks;
+                filteredTasks = tasks;
                 break;
         };
 
         return (
             <Card className={styles.container}>
-                <DragDropContext onDragEnd={this.onDragEnd}>
+                <DragDropContext onDragEnd={onDragEnd}>
                     <div>
                         <div className={styles.todoHeader}>
                             <h1 className={styles.todoHeaderTitle}>Список моих дел</h1>
                             <CalcTodo
-                                tasks={this.state.tasks}
-                                onClickFilteredTasks={this.onClickFilteredTasks}
-                                filtered={this.state.filtered}
+                                tasks={tasks}
+                                onClickFilteredTasks={onClickFilteredTasks}
+                                filtered={filtered}
                             />
                         </div>
                         <ItemList
                             filtered={filteredTasks}
-                            filteredValue={this.state.filtered}
-                            onClickDone={this.onClickDone}
-                            onClickImportant={this.onClickImportant}
-                            onClickDelete={this.onClickDelete} />
+                            filteredValue={filtered}
+                            onClickDone={onClickDone}
+                            onClickImportant={onClickImportant}
+                            onClickDelete={onClickDelete} />
 
-                        <Input items={this.state.tasks}
-                               classNameForInputRepeat={this.state.classNameForInputRepeat}
-                               onClickAdd={this.onClickAdd}
+                        <Input items={tasks}
+                               classNameForInputRepeat={classNameForInputRepeat}
+                               onClickAdd={onClickAdd}
                         />
                     </div>
                 </DragDropContext>
             </Card>
-        )}
+        )
 };
 
 export default Todo;
